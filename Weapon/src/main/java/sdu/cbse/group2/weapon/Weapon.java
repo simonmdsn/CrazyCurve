42e53e00ab1179b50b6360342788bb12ab4402d7
simon <simonmdsn@gmail.com>
2020-03-23 09:39:55 +0100

    META-INF files added to weapon.

--- Weapon/src/main/java/sdu/cbse/group2/weapon/Weapon.java
+++ Weapon/src/main/java/sdu/cbse/group2/weapon/Weapon.java
@@ -2,6 +2,7 @@ package sdu.cbse.group2.weapon;
 
 import lombok.Getter;
 import lombok.Setter;
+import sdu.cbse.group2.common.data.Entity;
 import sdu.cbse.group2.common.data.GameSprite;
 import sdu.cbse.group2.common.item.Item;
 
