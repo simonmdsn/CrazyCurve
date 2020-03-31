a19b62a123c36207782ad286e4b612812a691ec7
simon <simonmdsn@gmail.com>
2020-03-31 12:04:06 +0200

    Creates tails with processing service instead of a scheduled task. Should fix the concurrent method exception.

---
+++ CommonSnake/src/main/java/sdu/cbse/group2/commonsnake/TailProcess.java
@@ -0,0 +1,40 @@
+package sdu.cbse.group2.commonsnake;
+
+import sdu.cbse.group2.common.data.Entity;
+import sdu.cbse.group2.common.data.GameData;
+import sdu.cbse.group2.common.data.World;
+import sdu.cbse.group2.common.data.entityparts.PositionPart;
+import sdu.cbse.group2.common.services.IEntityProcessingService;
+
+public class TailProcess implements IEntityProcessingService {
+
+    private static final float DRAW_DISTANCE = 16;
+
+    private float distance(Entity entity1, Entity entity2) {
+        PositionPart positionPart1 = entity1.getPart(PositionPart.class);
+        PositionPart positionPart2 = entity2.getPart(PositionPart.class);
+        float dx = positionPart1.getX() - positionPart2.getX();
+        float dy = positionPart1.getY() - positionPart2.getY();
+        return (float) Math.sqrt(dx * dx + dy * dy);
+    }
+
+    private void createTail(World world, CommonSnake snake) {
+        Tail tail = new Tail(snake.getTailSprite());
+        PositionPart part = snake.getPart(PositionPart.class);
+        tail.add(new PositionPart(part.getX(), part.getY(), part.getRadians()));
+        snake.getTailList().add(tail);
+        world.addEntity(tail);
+    }
+
+    @Override
+    public void process(GameData gameData, World world) {
+        for (Entity commonSnake : world.getEntities(CommonSnake.class)) {
+            CommonSnake snake = ((CommonSnake) commonSnake);
+            if (snake.getTailList().isEmpty()) {
+                createTail(world, snake);
+            } else if (distance(snake, snake.getTailList().get(snake.getTailList().size() - 1)) > DRAW_DISTANCE) {
+                createTail(world, snake);
+            }
+        }
+    }
+}
