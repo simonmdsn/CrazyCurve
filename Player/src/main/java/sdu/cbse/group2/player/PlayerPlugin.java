1a113d9b49141b1d0ed9b7ed842cb6b5e4afc637
Joachim <joachimbulow@hotmail.dk>
2020-04-03 17:58:39 +0200

    settings state created, function for calculating center angle made

--- Player/src/main/java/sdu/cbse/group2/player/PlayerPlugin.java
+++ Player/src/main/java/sdu/cbse/group2/player/PlayerPlugin.java
@@ -9,14 +9,20 @@ import sdu.cbse.group2.common.services.IGamePluginService;
 import sdu.cbse.group2.commonsnake.CommonSnake;
 import sdu.cbse.group2.commonsnake.SnakeSPI;
 
+import java.util.Random;
+
 public class PlayerPlugin implements SnakeSPI, IGamePluginService {
 
     private CommonSnake player;
+    private Random random;
+    GameData gameData;
 
     @Override
     public void start(GameData gameData, World world) {
         player = createPlayerSnake();
         world.addEntity(player);
+        random = new Random();
+        this.gameData = gameData;
     }
 
     @Override
@@ -28,9 +34,9 @@ public class PlayerPlugin implements SnakeSPI, IGamePluginService {
     private CommonSnake createPlayerSnake() {
         float maxSpeed = 100;
         float rotationSpeed = 3;
-        float x = 200;
-        float y = 200;
-        float radians = 5;
+        float x = random.nextInt(gameData.getDisplayWidth());
+        float y = random.nextInt(gameData.getDisplayHeight());
+        float radians = 0;
 
         CommonSnake playerSnake = new CommonSnake(new GameSprite("player/player.png", 30, 30),new GameSprite("player/tail.png",30,30));
         playerSnake.add(new MovingPart(maxSpeed, rotationSpeed));
@@ -43,4 +49,12 @@ public class PlayerPlugin implements SnakeSPI, IGamePluginService {
     public CommonSnake create(GameData gameData, World world) {
         return null;
     }
+
+    private int getRadiansForSnake(int x, int y){
+        int centerX = gameData.getDisplayWidth() / 2;
+        int centerY = gameData.getDisplayHeight() / 2;
+        return 0;
+    }
 }
+
+
