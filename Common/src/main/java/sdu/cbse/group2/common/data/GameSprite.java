6259c27e87c9461fb3be1abb73647d5dd7c5b1b1
simon <simonmdsn@gmail.com>
2020-04-14 11:34:41 +0200

    A snake's head is drawn on top of the tails.

--- Common/src/main/java/sdu/cbse/group2/common/data/GameSprite.java
+++ Common/src/main/java/sdu/cbse/group2/common/data/GameSprite.java
@@ -1,10 +1,15 @@
 package sdu.cbse.group2.common.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class GameSprite {
 
     private final String imagePath;
     private final float height, width;
+    private int layer;
 }
