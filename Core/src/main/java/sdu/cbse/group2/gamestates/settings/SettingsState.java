4fbf2104c770510dc7ab524ecae6a6b02f80e36e
Benjamin <bemmamin@live.dk>
2020-04-16 23:52:50 +0200

    LibGDX table and position adjustment + auto-scaling.

--- Core/src/main/java/sdu/cbse/group2/gamestates/settings/SettingsState.java
+++ Core/src/main/java/sdu/cbse/group2/gamestates/settings/SettingsState.java
@@ -9,17 +9,20 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import sdu.cbse.group2.Game;
 import sdu.cbse.group2.gamestates.MenuState;
 import sdu.cbse.group2.gamestates.State;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class SettingsState extends State {
@@ -38,15 +41,12 @@ public class SettingsState extends State {
     }
 
     private void init() {
-        for (int i = 0; i < crazyCurveModules.size(); i++) {
-            final CrazyCurveModule crazyCurveModule = crazyCurveModules.get(i);
-            final TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-            textButtonStyle.font = new BitmapFont();
+        final Table table = new Table();
+        table.setFillParent(true);
+        for (final CrazyCurveModule crazyCurveModule : crazyCurveModules) {
+            final TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle(null, null, null, new BitmapFont());
             textButtonStyle.fontColor = crazyCurveModule.isActive() ? Color.GREEN : Color.RED;
             final TextButton textButton = new TextButton("(" + crazyCurveModule.getId() + ") " + crazyCurveModule.getName(), textButtonStyle);
-            textButton.getLabel().setAlignment(Align.left);
-            textButton.setSize(200, textButtonStyle.font.getCapHeight());
-            textButton.setPosition((int) (getGame().getGameData().getDisplayWidth() / 2.3D), (int) (getGame().getGameData().getDisplayHeight() / 1.3D) - 30 * i);
             textButton.addListener(new ClickListener() {
                 @Override
                 public void clicked(final InputEvent event, final float x, final float y) {
@@ -56,17 +56,22 @@ public class SettingsState extends State {
                     textButtonStyle.fontColor = crazyCurveModule.isActive() ? Color.GREEN : Color.RED;
                 }
             });
-            stage.addActor(textButton);
+            table.add(textButton).center().row();
         }
-        createBackButton();
+        final Function<TextButton, Float> textWidth = textButton -> textButton.getStyle().font.getBounds(textButton.getText()).width;
+        final float greatestWidth = Arrays.stream(table.getCells().toArray()).map(Cell::getActor).map(TextButton.class::cast).map(textWidth).max(Comparator.comparingDouble(Float::doubleValue)).orElse(0F);
+        Arrays.stream(table.getCells().toArray()).map(Cell::getActor).map(TextButton.class::cast).forEach(textButton -> textButton.padRight(greatestWidth - textWidth.apply(textButton)));
+        table.add(createBackButton());
+        stage.addActor(table);
         Gdx.input.setInputProcessor(stage);
     }
 
-    private void createBackButton() {
+    private ImageButton createBackButton() {
         final Texture backButtonTexture = getGame().getAssets().getAssetManager().get("MenuState/settings button.PNG"); //TODO Back image.
         Drawable backDrawable = new TextureRegionDrawable(new TextureRegion(backButtonTexture));
         final ImageButton backButton = new ImageButton(backDrawable);
         backButton.setSize(200, 80);
+        backButton.padTop(30);
         backButton.setPosition((int) (getGame().getGameData().getDisplayWidth() / 2D - backButton.getWidth() / 2D), 150);
         backButton.addListener(new ChangeListener() {
             @Override
@@ -75,7 +80,7 @@ public class SettingsState extends State {
                 dispose();
             }
         });
-        stage.addActor(backButton);
+        return backButton;
     }
 
     @Override
