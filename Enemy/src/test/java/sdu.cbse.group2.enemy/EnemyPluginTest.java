d455d64c11c8f761c80c9a9ac021c30a8fbf91a8
Dan Trong Thai <dthai18@student.sdu.dk>
2020-05-10 14:24:29 +0200

    Set up a Basic Testing class for EnemyPlugin.java.
    Problems
    * Maven Surefire?
    * What classes should be tested?

--- Enemy/src/test/java/sdu.cbse.group2.enemy/EnemyPluginTest.java
+++ Enemy/src/test/java/sdu.cbse.group2.enemy/EnemyPluginTest.java
@@ -1,16 +1,27 @@
 package sdu.cbse.group2.enemy;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class EnemyPluginTest {
 
     @BeforeEach
-    void setUp() {
+    void init() {
+    }
 
+    @Test
+    void failingTest() {
+        fail("Test failed");
+    }
+
+    @Test
+    @DisplayName("etc")
+    void successfulTest() {
     }
 
     @AfterEach
     void tearDown() {
     }
+
 }
