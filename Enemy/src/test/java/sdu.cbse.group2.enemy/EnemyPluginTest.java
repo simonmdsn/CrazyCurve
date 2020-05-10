4831bab2959b3a8a31553cd5765ea62d1bbdfb84
Dan Trong Thai <dthai18@student.sdu.dk>
2020-05-10 13:44:41 +0200

    Set up necessary dependencies and project structure to start JUnit tests.

---
+++ Enemy/src/test/java/sdu.cbse.group2.enemy/EnemyPluginTest.java
@@ -0,0 +1,16 @@
+package sdu.cbse.group2.enemy;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class EnemyPluginTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
